c5c288f0493b55417c603fcb32ae5d2c83bccb82
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2022-12-03 08:38:47 +0100

    add delete icon

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,10 +1,19 @@
 import { Box } from 'styles';
+import { Item, Text, Button } from 'components/ContactList.styled';
+import { AiFillCloseCircle } from 'react-icons/ai';
 
 export const ContactList = ({ contacts }) => {
   return contacts.length > 0 ? (
     <Box as="ul" textAlign="left">
       {contacts.map(({ id, name, number }) => {
-        return <li key={id}>{`${name}: ${number}`}</li>;
+        return (
+          <Item key={id}>
+            <Text>{`${name}: ${number}`}</Text>
+            <Button type="button">
+              <AiFillCloseCircle size="18px" color="white" fill="#00bcd5" />
+            </Button>
+          </Item>
+        );
       })}
     </Box>
   ) : (
