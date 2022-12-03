a9cb665de8b5ecd7a43517d42331da23bf73eca9
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2022-12-03 07:18:48 +0100

    add ContactForm styles

---
+++ src/components/ContactForm.styled.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const Label = styled.label`
+  display: block;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  line-height: 1;
+`;
+
+export const Input = styled.input`
+  display: block;
+  width: 236px;
+  line-height: 1;
+
+  &:last-of-type {
+    margin-bottom: 20px;
+  }
+`;
+
+export const Button = styled.button`
+  display: block;
+  margin: auto;
+  margin-bottom: 20px;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 25px;
+  color: #fff;
+  background-color: #00bcd5;
+  transition: 250ms ease-in-out;
+
+  &:hover {
+    background-color: #00a6bc;
+  }
+`;
