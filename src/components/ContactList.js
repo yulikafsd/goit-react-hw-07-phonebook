ea7cf5a2abc4d80de181014df506ca2baf3819bb
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2022-12-03 07:16:49 +0100

    add ContactList

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,8 +1,16 @@
-import { Component } from 'react';
 import { Box } from 'styles';
 
-export class ContactList extends Component {
-  render() {
-    return <Box></Box>;
-  }
-}
+export const ContactList = ({ contacts }) => {
+  return contacts.length > 0 ? (
+    <Box as="ul" textAlign="left">
+      {contacts.map(({ id, name, number }) => {
+        return <li key={id}>{`${name}: ${number}`}</li>;
+      })}
+    </Box>
+  ) : (
+    <Box fontSize={14}>
+      <p>Contact not found</p>
+      <p>Try another search name</p>
+    </Box>
+  );
+};
